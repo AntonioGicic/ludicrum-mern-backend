f8f55bee0c696e89f26c5500587b9173c95a1d4d
AntonioGicic <113938560+AntonioGicic@users.noreply.github.com>
2023-02-05 20:04:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -39,13 +39,6 @@ cron.schedule('* * 1 * * *', async () => {
     }
 });
 
-if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    app.use(express.static('../frontend/build'));
-
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
-}
-
 //API-s
 // app.get('/', (req, res) => {
 //     try {
